37853412fe448e5d4689c371e7fbe66beb0d5683
Muhammad-F336 <muhammad.f336s@gmail.com>
2025-08-30 21:08:49 +0500

    Approved About.js and App.js

--- 30-08-2025/src/App.js
+++ 30-08-2025/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+// import About from "./components/About.js";
 
 function App() {
   return (
     <>
       <Navbar title="Test" aboutText="AboutUS" linkText="Link" />
+      {/* <About /> */}
       <TextForm />
     </>
   );
