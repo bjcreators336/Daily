6bfa8b75c154af4ac0bcb83476c2ed234541fb65
BJ Creators 336 <BJ.Creators336@gmail.com>
2025-08-31 13:32:57 +0500

    Added all project files

--- 30-08-2025/src/App.js
+++ 30-08-2025/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
+import About from "./components/About.js";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
-import About from "./components/About.Js";
+// import TextForm from "./components/TextForm";
+// import About from "./components/About.js";
 
 function App() {
   return (
     <>
       <Navbar title="Test" aboutText="AboutUS" linkText="Link" />
+      {/* <About /> */}
       <About />
-      <TextForm />
+      {/* <TextForm /> */}
     </>
   );
 }
