97d681bbeea2b2c29c687490fd04f954dc369e21
Bj.creators <bj.creators336@gmail.com>
2025-09-02 08:21:56 +0500

    Including Enable Dark Mode feature in Text Analyzer

---
+++ 01-09-2025/test3.0/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar'
+import TextForm from './components/TextForm'
+
+
+function App() {
+  return (
+  
+     <>
+      <Navbar title="Test" aboutText="AboutUS" linkText="Link" />
+      {/* <About /> */}
+      {/* <About /> */}
+      <TextForm />
+    </>
+  );
+}
+
+export default App;
---
+++ 01-09-2025/test3.0/src/components/TextForm.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+
+  const HandleUpClick = () => {
+    setText(text.toUpperCase());
+  };
+  const HandleClearClick = () => {
+    setText("");
+  };
+  const HandleModifyClick = () => {
+    setText(
+      text.replace(/(^\s*\w|[.!? ]\s*\w)/g, (char) => char.toUpperCase())
+    );
+  };
+
+  const HandleLoClick = () => {
+    setText(text.toLowerCase());
+  };
+
+  const HandleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const Analyze = () => {
+    const Inns = document.querySelector(".Inns");
+    Inns.style.visibility = "visible";
+  };
+  const Analyze2 = () => {
+    const Inns = document.querySelector(".Inns");
+    Inns.style.visibility = "hidden";
+  };
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3"></div>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="20"
+              placeholder="Enter your text here"
+            value={text}
+            onChange={HandleOnChange}
+          ></textarea>
+          <button
+            className="btn btn-primary"
+            onClick={Analyze}
+            onDoubleClick={Analyze2}
+          >
+            Analyze
+          </button>
+          <button className="btn btn-primary mx-3" onClick={HandleUpClick}>
+            Convert To UpperCase
+          </button>
+          <button className="btn btn-primary mx-3" onClick={HandleLoClick}>
+            Convert To LowerCase
+          </button>
+          <button className="btn btn-primary mx-3" onClick={HandleClearClick}>
+            Clear Text
+          </button>
+          <button className="btn btn-primary mx-3" onClick={HandleModifyClick}>
+            Modify Text
+          </button>
+        </div>
+        <div className="Inns" style={{ visibility: "hidden" }}>
+          <div className="container">
+            <h2>Text Summary</h2>
+            <p>
+              Text contains <b>{text.split(" ").length}</b> Words and{" "}
+              <b>{text.length} </b>
+              Letters <br /> An average of{" "}
+              <b>{0.008 * text.split(" ").length} </b>
+              Minutes to read
+            </p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
